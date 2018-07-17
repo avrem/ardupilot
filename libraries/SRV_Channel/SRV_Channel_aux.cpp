4784061c00052e38549495f0b2e502dde8c30e93
Alexander Remizov <avremizov@gmail.com>
2018-07-17 14:57:06 +0300

    support folding propellers

--- libraries/SRV_Channel/SRV_Channel_aux.cpp
+++ libraries/SRV_Channel/SRV_Channel_aux.cpp
@@ -132,6 +132,9 @@ void SRV_Channel::aux_servo_function_setup(void)
         // fixed wing throttle
         set_range(100);
         break;
+    case k_unused1:
+        set_range(100);
+        break;
     default:
         break;
     }
