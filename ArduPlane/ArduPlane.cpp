3c52b6c02a8be7b484a49dd6a16bf75226fac239
Alexander Remizov <avremizov@gmail.com>
2018-10-06 20:25:22 +0300

    Plane: add AP_Generator

--- ArduPlane/ArduPlane.cpp
+++ ArduPlane/ArduPlane.cpp
@@ -57,6 +57,7 @@ const AP_Scheduler::Task Plane::scheduler_tasks[] = {
     SCHED_TASK(update_notify,          50,    300),
     SCHED_TASK(read_rangefinder,       50,    100),
     SCHED_TASK_CLASS(AP_ICEngine, &plane.g2.ice_control, update, 10, 100),
+    SCHED_TASK_CLASS(AP_Generator, &plane.g2.generator, update, 50, 100),
     SCHED_TASK(compass_cal_update,     50,    50),
     SCHED_TASK(accel_cal_update,       10,    50),
 #if OPTFLOW == ENABLED
--- ArduPlane/commands_logic.cpp
+++ ArduPlane/commands_logic.cpp
@@ -232,7 +232,7 @@ bool Plane::start_command(const AP_Mission::Mission_Command& cmd)
         break;
 
     case MAV_CMD_DO_ENGINE_CONTROL:
-        plane.g2.ice_control.engine_control(cmd.content.do_engine_control.start_control,
+        plane.g2.generator.engine_control(cmd.content.do_engine_control.start_control,
                                             cmd.content.do_engine_control.cold_start,
                                             cmd.content.do_engine_control.height_delay_cm*0.01f);
         break;
