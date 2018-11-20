86379e1a88d69d8f2ade30b247257fc47d7ed177
Alexander Remizov <avremizov@gmail.com>
2018-11-20 16:17:43 +0300

    Revert "Plane: Enforce THR_MIN/THR_MAX in VTOL modes"
    This reverts commit 624aef401d01fd2f9f2cf2d2ae10703ef702efb0.

--- ArduPlane/servos.cpp
+++ ArduPlane/servos.cpp
@@ -378,8 +378,7 @@ void Plane::set_servos_controlled(void)
         SRV_Channels::set_output_scaled(SRV_Channel::k_throttle, channel_throttle->get_control_in_zero_dz());
     } else if (quadplane.in_vtol_mode()) {
         // ask quadplane code for forward throttle
-        SRV_Channels::set_output_scaled(SRV_Channel::k_throttle, 
-            constrain_int16(quadplane.forward_throttle_pct(), min_throttle, max_throttle));
+        SRV_Channels::set_output_scaled(SRV_Channel::k_throttle, quadplane.forward_throttle_pct());
     }
 
     // suppress throttle when soaring is active
