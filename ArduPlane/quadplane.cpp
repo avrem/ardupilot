913fa0e204cf8ce915190a2dd238588bba78d562
Alexander Remizov <avremizov@gmail.com>
2019-07-28 02:28:10 +0300

    TiltQuad: proper forward throttle limits

--- ArduPlane/quadplane.cpp
+++ ArduPlane/quadplane.cpp
@@ -2464,9 +2464,9 @@ int8_t QuadPlane::forward_throttle_pct(void)
     fwd_vel_error /= MAX(plane.aparm.airspeed_max, 5);
 
     // add in a component from our current pitch demand. This tends to
-    // move us to zero pitch. Assume that LIM_PITCH would give us the
+    // move us to zero pitch. Assume that ANGLE_MAX would give us the
     // WP nav speed.
-    fwd_vel_error -= (wp_nav->get_speed_xy() * 0.01f) * plane.nav_pitch_cd / (float)plane.aparm.pitch_limit_max_cd;
+    fwd_vel_error -= (wp_nav->get_speed_xy() * 0.01f) * plane.nav_pitch_cd / (float)aparm.angle_max;
 
     if (should_relax() && vel_ned.length() < 1) {
         // we may be landed
@@ -2477,9 +2477,15 @@ int8_t QuadPlane::forward_throttle_pct(void)
     // integrator as throttle percentage (-100 to 100)
     vel_forward.integrator += fwd_vel_error * deltat * vel_forward.gain * 100;
 
-    // inhibit reverse throttle and allow petrol engines with min > 0
-    int8_t fwd_throttle_min = (plane.aparm.throttle_min <= 0) ? 0 : plane.aparm.throttle_min;
-    vel_forward.integrator = constrain_float(vel_forward.integrator, fwd_throttle_min, plane.aparm.throttle_max);
+    if (is_tiltquad() && tilt.max_angle_deg > 0) {
+        float fwd_tilt_throttle_max = 50 * (aparm.angle_max * 0.01f) / tilt.max_angle_deg;
+        vel_forward.integrator = constrain_float(vel_forward.integrator, 0, fwd_tilt_throttle_max);
+    }
+    else {
+        // inhibit reverse throttle and allow petrol engines with min > 0
+        int8_t fwd_throttle_min = (plane.aparm.throttle_min <= 0) ? 0 : plane.aparm.throttle_min;
+        vel_forward.integrator = constrain_float(vel_forward.integrator, fwd_throttle_min, plane.aparm.throttle_max);
+    }
     
     // If we are below alt_cutoff then scale down the effect until it turns off at alt_cutoff and decay the integrator
     float alt_cutoff = MAX(0,vel_forward_alt_cutoff);
